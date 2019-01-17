0f843aa5a3f4be75045415a72e9d331470f3a047
“Andrew <andrewwoods88@gmail.com>
2019-01-17 13:28:44 -0500

    new and edit form, errors shared partial

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,8 +1,14 @@
 class PlaysController < ApplicationController
   def index
+    @plays = Play.all
   end
 
   def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(play_params)
   end
 
   def edit
@@ -13,4 +19,12 @@ class PlaysController < ApplicationController
 
   def _form
   end
+
+  private
+
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
+  end
+
+  
 end
