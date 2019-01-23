423a68d0fbfc09a7e7682921d2cd71667e8be351
“Andrew <andrewwoods88@gmail.com>
2019-01-23 15:52:20 -0500

    finished paperclip uploading images

--- app/models/play.rb
+++ app/models/play.rb
@@ -2,6 +2,6 @@ class Play < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :play_img, styles: { :play_index: "250x350>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :play_img, styles: { medium: "300x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/           
 end
